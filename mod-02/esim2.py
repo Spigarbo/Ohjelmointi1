14333d783d7afdb3200237e7c4bf606f91840cc4
Spigarbo <miio.arasola@gmail.com>
2025-08-19 15:31:22 +0300

    Tehtävät ja esimerkit

---
+++ mod-02/esim2.py
@@ -0,0 +1,18 @@
+banaani = input("Syötä banaanien määrä kiloina: ")
+omena = input("Syötä omenien määrä kiloina: ")
+appelsiini = input("Syötä appelsiinien määrä kiloina: ")
+
+banaani_kilohinta = 2.85
+omena_kilohinta = 3.15
+appelsiini_kilohinta = 4.05
+
+banaani_hinta = float(banaani) * banaani_kilohinta
+omena_hinta = float(omena) * omena_kilohinta
+appelsiini_hinta = float(appelsiini) * appelsiini_kilohinta
+summa = banaani_hinta + omena_hinta + appelsiini_hinta
+
+print("Ostosten yhteenveto:")
+print(f"Banaanit: {banaani_hinta:.2f}€")
+print(f"Omenat: {omena_hinta:.2f}€")
+print(f"Appelsiinit: {appelsiini_hinta:.2f}€")
+print(f"Yhteensä: {summa}€")
---
+++ mod-02/t1.py
@@ -0,0 +1,11 @@
+käyttäjä = input("Kirjoita nimesi: ")
+
+print("Moro " + käyttäjä + "!")
+
+print(1 + 4 + 9)
+
+
+fahrenheit_str = input("Anna lämpötila Fahrenheit-asteina: ")
+fahrenheit = float(fahrenheit_str)
+celsius = (fahrenheit-32)*5/9
+print("Lämpötila celsius-asteina: "+ str(celsius))
